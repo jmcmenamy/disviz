b86c39071101e7f9609a25ecd88618fb595992e6
Josiah McMenamy <josiah.mcmenamy@gmail.com>
2025-04-02 13:36:00 -0400

    Do some import trickery to get all js code in global scope; make proxies for jquery and d3 for ease of implementation

--- index.js
+++ index.js
@@ -1,18 +1,83 @@
 // server.js
-const express = require('express');
-const http = require('http');
-const WebSocket = require('ws');
-const fs = require('fs').promises;
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+import express from 'express';
+import * as http from 'http'
+import { WebSocketServer } from 'ws';
+import * as vm from 'vm'
+import * as path from 'path'
+import fs from 'fs';
+// const express = require('express');
+// const http = require('http');
+// const WebSocket = require('ws');
+// const vm = require('vm');
+// const path = require('path');
+// const fs = require('fs').promises;
+function loadGlobalScript(filename) {
+  const filePath = path.join(__dirname, filename);
+  const code = fs.readFileSync(filePath, {encoding: 'utf8'} );
+  // Run the code in the current context, so any variables declared at the top level
+  // (without using module exports) will be added to the global object.
+  vm.runInThisContext(code, { filename: filePath });
+}
+
+// load all js_server files in global context (for ease of implementation)
+for (const path of [
+  "js_server/dev.js",
+  "js_server/searchBar.js","js_server/clusterer.js",
+  "js_server/graph/graphEvent.js","js_server/graph/abstractGraph.js","js_server/graph/abstractNode.js","js_server/graph/graphTraversal.js","js_server/graph/dfsGraphTraversal.js",
+  "js_server/builder/builderGraph.js","js_server/builder/builderNode.js","js_server/builder/graphBuilder.js","js_server/builder/graphBuilderHost.js","js_server/builder/graphBuilderNode.js",
+  "js_server/logEventMatcher/lemAST.js","js_server/logEventMatcher/lemInterpreter.js","js_server/logEventMatcher/lemParser.js","js_server/logEventMatcher/lemToken.js","js_server/logEventMatcher/lemTokenizer.js","js_server/logEventMatcher/logEventMatcher.js",
+  "js_server/model/logEvent.js","js_server/model/modelGraph.js","js_server/model/modelNode.js","js_server/model/parser.js","js_server/model/vectorTimestamp.js","js_server/model/vectorTimestampSerializer.js",
+  "js_server/motifFinder/motif.js","js_server/motifFinder/motifFinder.js","js_server/motifFinder/motifDrawer.js","js_server/motifFinder/broadcastGatherFinder.js","js_server/motifFinder/customMotifFinder.js","js_server/motifFinder/motifGroup.js","js_server/motifFinder/motifNavigator.js","js_server/motifFinder/requestResponseFinder.js","js_server/motifFinder/textQueryMotifFinder.js",
+  "js_server/shiviz.js",
+  "js_server/transform/transformation.js","js_server/transform/collapseSequentialNodesTransformation.js","js_server/transform/hideHostTransformation.js","js_server/transform/highlightHostTransformation.js","js_server/transform/highlightMotifTransformation.js","js_server/transform/showDiffTransformation.js","js_server/transform/transformer.js",
+  "js_server/util/exception.js","js_server/util/regexp.js","js_server/util/util.js",
+  "js_server/visualization/controller.js","js_server/visualization/global.js","js_server/visualization/hostPermutation.js","js_server/visualization/layout.js","js_server/visualization/view.js","js_server/visualization/visualEdge.js","js_server/visualization/visualGraph.js","js_server/visualization/visualNode.js","js_server/visualization/abbreviation.js",
+  ]) {
+  loadGlobalScript(path)
+}
+
+// console.log(ModelGraph, "testing")
+// // Example: Load multiple files whose declarations will end up in the global scope.
+// 'file1.js',// 'file2.js',
+// first create no op proxies for jquery and d3 (for ease of implementation)
+// Create a Proxy that intercepts all property accesses and function calls
+const handler = {
+  // Intercepts property access, e.g. $().testing
+  get(target, prop) {
+    return proxy;
+  },
+  // Intercepts function calls, e.g. $()
+  apply(target, thisArg, argumentsList) {
+    return proxy;
+  }
+};
+
+// Create a Proxy using the no-op function and our handler.
+// We call it "proxy" so that property accesses return the same object.
+let proxy
+proxy = new Proxy(() => proxy, handler);
+
+const $ = proxy
+const d3 = proxy
 const app = express();
 
 // Serve static files from the 'public' directory
 app.use(express.static(__dirname));
 
+// defines the file that will be continuously read from
+let filehandle = undefined
+let stats = undefined
+
 const server = http.createServer(app);
 
 // Create a WebSocket server on the path '/ws'
-const wss = new WebSocket.Server({ server, path: '/ws' });
+const wss = new WebSocketServer({ server, path: '/ws'});
 
 wss.on('connection', (ws) => {
   console.log('Client connected');
@@ -24,9 +89,10 @@ wss.on('connection', (ws) => {
         switch (message.type) {
             case "filePathRequest":
                 console.log("Got filePathRequest", message)
+                await setNewFile(message.filePath)
                 ws.send(JSON.stringify({
                   id: message.id,
-                  logs: await newFile(message.filePath, 0, message.numBytes)
+                  logs: await newFile(0, message.numBytes)
                 }), (err) => {
                   if (err === null) {
                     return
@@ -48,6 +114,16 @@ wss.on('connection', (ws) => {
   });
 });
 
+async function setNewFile(filename) {
+  if (filehandle !== undefined) {
+    await filehandle.close()
+  }
+  const fileText = fs.readFileSync(filename, { encoding: 'utf8' })
+  filehandle = await fs.promises.open(filename, 'r');
+  stats = await filehandle.stat();
+  // need to sort it and write it back so we only ever send valid subsets of the log events
+}
+
 /**
  * Reads a portion of a file between given byte offsets,
  * expanding the range until complete lines (i.e. until newline characters)
@@ -58,10 +134,10 @@ wss.on('connection', (ws) => {
  * @param {number} endOffset - The desired ending byte offset.
  * @returns {Promise<string>} - Resolves to the file data as a UTF-8 string containing complete lines.
  */
-async function newFile(filename, startOffset, endOffset) {
+async function newFile(startOffset, endOffset) {
   // Open the file for reading.
-  const filehandle = await fs.open(filename, 'r');
-  const stats = await filehandle.stat();
+  // const filehandle = await fs.open(filename, 'r');
+  // const stats = await filehandle.stat();
   const fileSize = stats.size;
 
   // --- Adjust the start offset backwards to a newline ---
@@ -129,7 +205,6 @@ async function newFile(filename, startOffset, endOffset) {
   const readLength = adjustedEnd - adjustedStart;
   const finalBuffer = Buffer.alloc(readLength);
   await filehandle.read(finalBuffer, 0, readLength, adjustedStart);
-  await filehandle.close();
   console.log("Returninig");
   return finalBuffer.toString('utf8');
 }
